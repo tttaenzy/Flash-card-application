93363d6084818f3cdd64f6ab495134e474ded810
tttaenzy <tenzinchoeying91@gmail.com>
2023-11-04 13:23:38 +0530

    second commit

--- main.py
+++ main.py
@@ -9,14 +9,10 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front)
-canvas.grid(row=0, column=0, columnspan=2)
+canvas.create_text(400,150,text="French", font=("Ariel", 40, "italic"))
+canvas.create_text(400,263,text="trouve", font=("Ariel", 60, "bold"))
+canvas.grid(row=0,column=0,columnspan=2)
 
-# -----Label----------
-title = Label(text="French", font=("Ariel", 40, "italic"), bg="white")
-title.place(x=300, y=150)
-
-word = Label(text="trouve", font=("Ariel", 60, "bold"), bg="white")
-word.place(x=300, y=263)
 
 # ------Button------------
 right = PhotoImage(file="images/right.png")
