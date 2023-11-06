90e0946843dd425cac83923d27a98e8a0ddf8257
tttaenzy <tenzinchoeying91@gmail.com>
2023-11-06 18:33:26 +0530

    Last commit

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ from tkinter import *
 import pandas as pd
 import random
 
-
-#-------Create new flash card
-data=pd.read_csv("data/french_words.csv")
-to_learn=data.to_dict(orient="records")
 current_card={}
+to_learn={}
+#-------Create new flash card
+try:
+    data=pd.read_csv("data/known_word.csv")
+except FileNotFoundError:
+    original=pd.read_csv("data/french_words.csv")
+    to_learn=original.to_dict(orient="records")
+else:
+    to_learn=data.to_dict(orient="records")
+
 def next_card():
     global current_card,flip_timer
     window.after_cancel(flip_timer)
     current_card=random.choice(to_learn)
-    current=current_card["French"]
     canvas.itemconfig(french,text="French",fill="black")
-    canvas.itemconfig(meaning,text=current,fill="black")
+    canvas.itemconfig(meaning,text=current_card["French"],fill="black")
     canvas.itemconfig(card_background,image=card_front)
-    flip_timer=window.after(2000,func=flip_card)
+    flip_timer=window.after(3000,func=flip_card)
 
 
 def flip_card():
@@ -24,12 +29,17 @@ def flip_card():
     canvas.itemconfig(meaning,text=current_card["English"],fill="white")
     canvas.itemconfig(card_background,image=card_back)
 
+def right_button():
+    to_learn.remove(current_card)
+    data=pd.DataFrame(to_learn)
+    data.to_csv("data/known_word.csv",index=False)
+    next_card()
 # --------------------UI interface------------
 window = Tk()
 window.title("Flash card")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-flip_timer=window.after(2000,func=flip_card)
+flip_timer=window.after(3000,func=flip_card)
 # ----- image upload
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front = PhotoImage(file="images/card_front.png")
@@ -42,7 +52,7 @@ canvas.grid(row=0,column=0,columnspan=2)
 
 # ------Button------------
 right = PhotoImage(file="images/right.png")
-rightButton = Button(image=right, highlightthickness=0, command=next_card)
+rightButton = Button(image=right, highlightthickness=0, command=right_button)
 rightButton.grid(row=1, column=0)
 
 wrong = PhotoImage(file="images/wrong.png")
